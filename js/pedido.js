228122a96839370a1a37ee4a7ca5dee880bcb99a
Arturo Sarmiento <Arturosarmientopaszor@gmail.com>
2017-03-01 13:20:03 +0100

    cambios

--- js/pedido.js
+++ js/pedido.js
@@ -25,38 +25,47 @@ function Pedido(fechaPedido,
 	this.getFechaPedido = function () {
 		return this.fechaPedido;
 	};
+
 	// Metodo para conocer los articuloa
 	this.getTotalArticulo = function () {
 		return this.totalArticulos;
 	};
+
 	// Metodo para conocer el pedido total
 	this.getTotalPedido = function () {
 		return this.totalPedido;
 	};
+
 	// Metodo para conocer la direccion de entrega
 	this.getDireccionEntrega = function () {
 		return this.direccionEntrega;
 	};
+
 	// Metodo para conocer le direccion de facturacion
 	this.getDireccionFacturacion = function () {
 		return this.direccionFacturacion;
 	};
+
 	// Metodo para poder cambiar la fecha de pedido
 	this.setFechaPedido = function (fechaPedido) {
 		this.fechaPedido = fechaPedido;
 	};
+
 	// Metodo para poder cambiar la direccion de facturacion
 	this.setTotalArticulo = function (totalArticulos) {
 		this.totalArticulos = totalArticulos;
 	};
-		// Metodo para poder cambiar 
+
+		// Metodo para poder cambiar el pedido
 	this.setTotalPedido = function (totalPedido) {
 		this.totalPedido = totalPedido;
 	};
+
 	// Metodo para poder cambiar la direccion de entrega
 	this.setDireccionEntrega = function (direccionEntrega) {
 		this.direccionEntrega = direccionEntrega;
 	};
+
 		// Metodo para poder cambiar la direccion de facturacion
 	this.setDireccionFacturacion = function (direccionFacturacion) {
 		this.direccionFacturacion = direccionFacturacion;
